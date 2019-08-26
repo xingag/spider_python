d99ef050b455f0703f62cd393f5a24bbfade8aa1
xingag <xinganguo@gmail.com>
2019-08-26 23:12:48 +0800

    使用Python定位到女朋友的位置

---
+++ 获取女友的位置/main.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: v1.0
+@author: xag
+@license: Apache Licence
+@contact: xinganguo@gmail.com
+@site: http://www.xingag.top
+@software: PyCharm
+@file: meta_picture.py.py
+@time: 2019-08-23 16:23
+@description：高德坐标拾取网站：https://lbs.amap.com/console/show/picker
+"""
+
+import os
+import exifread
+from decimal import Decimal
+from position_utils import *
+import requests
+import json
+import datetime
+
+
+#  pip3 install exifread
+
+
+class Location(object):
+
+    def __init__(self, image_path):
+        self.img_path = image_path
+
+        self.api_key = "你申请的AK"
+
+        self.url_get_position = 'https://restapi.amap.com/v3/geocode/regeo?key={}&location={}'
+
+    def run(self):
+        coordinate = self.__get_image_ability()
+
+        print(f'获取到经度、纬度是:{coordinate}')
+
+        if not coordinate:
+            return
+
+        # 根据经度和纬度，获取到详细地址
+        address = self.__get_address(coordinate)
+
+        # 检验坐标值
+        # https://lbs.amap.com/console/show/picker
+        print(f'你女朋友当前位置在:{address}')
+
+    def __get_address(self, location):
+        """
+        根据坐标得到详细地址
+        :param location: 经纬度值
+        :return:
+        """
+        resp = requests.get(self.url_get_position.format(self.api_key, location))
+
+        location_data = json.loads(resp.text)
+
+        address = location_data.get('regeocode').get('formatted_address')
+
+        return address
+
+    def __format_lati_long_data(self, data):
+        """
+        对经度和纬度数据做处理，保留6位小数
+        :param data: 原始经度和纬度值
+        :return:
+        """
+        # 删除左右括号和空格
+        data_list_tmp = str(data).replace('[', '').replace(']', '').split(',')
+        data_list = [data.strip() for data in data_list_tmp]
+
+        # 替换秒的值
+        data_tmp = data_list[-1].split('/')
+
+        # 秒的值
+        data_sec = int(data_tmp[0]) / int(data_tmp[1]) / 3600
+
+        # 替换分的值
+        data_tmp = data_list[-2]
+
+        # 分的值
+        data_minute = int(data_tmp) / 60
+
+        # 度的值
+        data_degree = int(data_list[0])
+
+        # 由于高德API只能识别到小数点后的6位
+        # 需要转换为浮点数，并保留为6位小数
+        result = "%.6f" % (data_degree + data_minute + data_sec)
+        return float(result)
+
+    def __get_image_ability(self):
+        """
+        获取图片的属性值，包含：经纬度、拍摄时间等
+        :param picture_name:
+        :return:
+        """
+
+        # 利用exifread库，读取图片的属性
+        img_exif = exifread.process_file(open(self.img_path, 'rb'))
+
+        # 能够读取到属性
+        if img_exif:
+            # 纬度数
+            latitude_gps = img_exif['GPS GPSLatitude']
+
+            # N,S 南北纬方向
+            latitude_direction = img_exif['GPS GPSLatitudeRef']
+
+            # 经度数
+            longitude_gps = img_exif['GPS GPSLongitude']
+
+            # E,W 东西经方向
+            longitude_direction = img_exif['GPS GPSLongitudeRef']
+
+            # 拍摄时间
+            take_time = img_exif['EXIF DateTimeOriginal']
+
+            is_lie = self.judge_time_met(take_time)
+
+            if is_lie:
+                print('很遗憾的通知你，你的女朋友在撒谎！！！')
+                return
+
+                # 纬度、经度、拍摄时间
+            if latitude_gps and longitude_gps and take_time:
+
+                # 对纬度、经度值原始值作进一步的处理
+                latitude = self.__format_lati_long_data(latitude_gps)
+                longitude = self.__format_lati_long_data(longitude_gps)
+
+                # print(f'{longitude},{latitude}')
+
+                # 注意：由于gps获取的坐标在国内高德等主流地图上逆编码不够精确，这里需要转换为火星坐标系
+                location = wgs84togcj02(longitude, latitude)
+
+                return f'{location[0]},{location[1]}'
+            else:
+                print(f'获取的图片数据属性不完整')
+                return ''
+        else:
+            print('抱歉，图片不是原图，没法获取到图片属性。')
+            return ''
+
+    def judge_time_met(self, take_time):
+        """
+        判断拍摄时间是否是在今天
+        :param take_time:
+        :return:
+        """
+        # 拍摄时间
+        format_time = str(take_time).split(" ")[0].replace(":", "-")
+
+        # 当天日期
+        today = str(datetime.date.today())
+
+        if format_time == today:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    # 女朋友发过来的图片【原图】
+    location = Location('./picture/11441566648796_.pic_hd.jpg')
+
+    # 找到女朋友的地理位置
+    location.run()
---
+++ 获取女友的位置/position_utils.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: v1.0
+@author: xag
+@license: Apache Licence
+@contact: xinganguo@gmail.com
+@site: http://www.xingag.top
+@software: PyCharm
+@file: position_utils.py
+@time: 2019-08-23 17:44
+@description：坐标转换
+"""
+
+# -*- coding: utf-8 -*-
+import json
+import math
+
+x_pi = 3.14159265358979324 * 3000.0 / 180.0
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+ee = 0.00669342162296594323  # 扁率
+
+
+def wgs84togcj02(lng, lat):
+    """
+    WGS84转GCJ02(火星坐标系)
+    :param lng:WGS84坐标系的经度
+    :param lat:WGS84坐标系的纬度
+    :return:
+    """
+    if out_of_china(lng, lat):  # 判断是否在国内
+        return lng, lat
+    dlat = transformlat(lng - 105.0, lat - 35.0)
+    dlng = transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [mglng, mglat]
+
+
+def gcj02towgs84(lng, lat):
+    """
+    GCJ02(火星坐标系)转GPS84
+    :param lng:火星坐标系的经度
+    :param lat:火星坐标系纬度
+    :return:
+    """
+    if out_of_china(lng, lat):
+        return lng, lat
+    dlat = transformlat(lng - 105.0, lat - 35.0)
+    dlng = transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [lng * 2 - mglng, lat * 2 - mglat]
+
+
+def transformlat(lng, lat):
+    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+          0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lat * pi) + 40.0 *
+            math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 *
+            math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+
+def transformlng(lng, lat):
+    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+          0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lng * pi) + 40.0 *
+            math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 *
+            math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+
+def out_of_china(lng, lat):
+    """
+    判断是否在国内，不在国内不做偏移
+    :param lng:
+    :param lat:
+    :return:
+    """
+    if lng < 72.004 or lng > 137.8347:
+        return True
+    if lat < 0.8293 or lat > 55.8271:
+        return True
+    return False
